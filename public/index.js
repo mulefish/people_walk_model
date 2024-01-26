832a94200a6ea78f08cb2670ca28a18ea8b16804
mulefish <pmontgomery@lululemon.com>
2024-01-26 05:50:33 -0800

    work it

--- public/index.js
+++ public/index.js
@@ -1,33 +1,121 @@
-let canvas = document.getElementById('myCanvas');
-let context = canvas.getContext('2d');
-let objects = [];
-let steps = 0;
 
-const H = window.innerHeight * 0.66;
-const W = window.innerWidth;
-const fifthW = W / 5
-const positionA = { x: fifthW, y: H / 2 };
-const positionB = { x: fifthW * 2, y: H / 2 };
-const positionC = { x: fifthW * 3, y: H / 2 };
-const maxSpeed = 6; // Maximum speed for the balls
+let agents = [];
+let numAgents = 6;
+let numSteps = 1000;
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+const maxSpeed = 6;
+let loops = 0 
+let img = new Image();
+const millisecond_pause = 100
 
-function init() {
-    canvas.width = W;
-    canvas.height = H;
+const positionFitting = { x: 580, y: 118 };
+const positionMen = { x: 845, y: 161 };
+const positionEntrance = { x: 960, y: 430 };
+const positionCheckout = { x: 800, y: 520 };
+const positionWomen = { x: 550, y: 428 };
+const leastX = 605
+const mostX = 925
+const leastY = 128
+const mostY = 480
+
+function getRandomNumberBetween(min, max) {
+    return Math.random() * (max - min + 1) + min;
+}
+function isCloseEnough(agent, target, threshold = 50) {
+    const distance = Math.sqrt(Math.pow(agent.x - target.x, 2) + Math.pow(agent.y - target.y, 2));
+    return distance <= threshold;
 }
 
-for (let i = 0; i < 10; i++) {
-    objects.push({
-        x: 10 + Math.random() * (W - 20),
-        y: 10 + Math.random() * (H - 20),
-        speedX: Math.random() * 10 - 5,
-        speedY: Math.random() * 10 - 5,
-        number: i,
-        attractionToA: Math.random(),
-        attractionToB: Math.random(),
-        attractionToC: Math.random()
-    });
+
+let areas = [] 
+let isSimulationRunning = false;
+let interval;
+
+// Initialize agents and start the simulation for the first time
+function startSimulation() {
+    if (!isSimulationRunning) {
+        isSimulationRunning = true;
+        initAgents();
+        continueSimulation();
+    }
 }
+
+
+
+// Function to resume or continue the simulation
+function continueSimulation() {
+    startTimer()
+    if (!isSimulationRunning) {
+        isSimulationRunning = true;
+        interval = setInterval(() => {
+            updateAgents();
+        }, millisecond_pause);
+    }
+}
+
+function pauseSimulation() {
+    console.log("pauseSimulation")
+
+    if (isSimulationRunning) {
+        clearInterval(interval);
+        pauseTimer()
+        isSimulationRunning = false;
+    }
+}
+
+document.getElementById("continueButton").addEventListener("click", function() {
+    if (isSimulationRunning) {
+        pauseSimulation();
+        this.textContent = 'Continue Simulation'; // Change button text to "Continue Simulation"
+    } else {
+        continueSimulation();
+        this.textContent = 'Pause Simulation'; // Change button text to "Pause Simulation"
+    }
+});
+
+
+function drawLetters() {
+    ctx.font = '48px Arial';
+    const myBlack = 'rgba(0, 0, 0, 0.9)';
+
+    ctx.fillStyle = myBlack
+
+    ctx.fillText('F', positionFitting.x, positionFitting.y);
+    ctx.fillText('M', positionMen.x, positionMen.y);
+    ctx.fillText('E', positionEntrance.x, positionEntrance.y);
+    ctx.fillText('C', positionCheckout.x, positionCheckout.y);
+    ctx.fillText('W', positionWomen.x, positionWomen.y);
+
+
+}
+
+img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    drawGrids();
+    startSimulation();
+};
+img.src = 'lulustore.png';
+
+
+function initAgents() {
+    for (let i = 0; i < numAgents; i++) {
+        agents.push({
+            x: getRandomNumberBetween(leastX, mostX),
+            y: getRandomNumberBetween(leastY, mostY),
+            speedX: Math.random() * 10 - 5,
+            speedY: Math.random() * 10 - 5,    
+            number: i,
+            target: null,
+            nextTarget: null,
+            isWaiting: Math.random() > 0.9 ? true : false 
+        });
+    }
+}
+
+
 
 function applyAttraction(obj, target, attractionWeight) {
     let dirX = target.x - obj.x;
@@ -36,11 +124,9 @@ function applyAttraction(obj, target, attractionWeight) {
     dirX /= length;
     dirY /= length;
 
-    // Increase the attraction effect
     obj.speedX += dirX * attractionWeight * 0.5;
     obj.speedY += dirY * attractionWeight * 0.5;
     
-    // Limit the speed to maxSpeed
     let speed = Math.sqrt(obj.speedX * obj.speedX + obj.speedY * obj.speedY);
     if (speed > maxSpeed) {
         obj.speedX = (obj.speedX / speed) * maxSpeed;
@@ -49,83 +135,170 @@ function applyAttraction(obj, target, attractionWeight) {
 }
 
 
-function drawObjects() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+function addAgentAtEntrance() {
+    const newAgent = {
+        x: positionEntrance.x,
+        y: positionEntrance.y,
+        speedX: Math.random() * 10 - 5,
+        speedY: Math.random() * 10 - 5,
+        number: agents.length,
+        target: null,
+        nextTarget: null
+    };
+    agents.push(newAgent);
+}
+
+
+function updateAgents() {
+    loops++;
+    document.getElementById("steps").innerHTML = loops;
+    
+    if (Math.random() < 0.05) { // Adjust this probability as needed
+        addAgentAtEntrance();
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0);
+    drawGrids();
     drawLetters();
 
-    objects.forEach(function (obj) {
-        // Apply attraction forces to each target
-        applyAttraction(obj, positionA, obj.attractionToA);
-        applyAttraction(obj, positionB, obj.attractionToB);
-        applyAttraction(obj, positionC, obj.attractionToC);
+    for (let i = agents.length - 1; i >= 0; i--) {
+        let agent = agents[i];
 
-        // Determine the favorite letter based on highest attraction
-        let maxAttraction = Math.max(obj.attractionToA, obj.attractionToB, obj.attractionToC);
-        let target;
-        if (maxAttraction === obj.attractionToA) {
-            target = positionA;
-        } else if (maxAttraction === obj.attractionToB) {
-            target = positionB;
-        } else {
-            target = positionC;
+        let targets = [positionFitting, positionMen, positionWomen, positionCheckout];
+        
+        if (agent.nextTarget === 'entrance') {
+            agent.target = positionEntrance;
+        } else if (!agent.target || isCloseEnough(agent, agent.target)) {
+            if (agent.target === positionCheckout) {
+                agent.nextTarget = 'entrance';
+            } else {
+                agent.target = targets[Math.floor(Math.random() * targets.length)];
+            }
         }
 
-        // // Draw a line to the favorite letter
-        // context.beginPath();
-        // context.moveTo(obj.x, obj.y);
-        // context.lineTo(target.x, target.y);
-        // context.strokeStyle = 'gray';
-        // context.stroke();
+        applyAttraction(agent, agent.target, 1);
 
-        if (obj.x <= 0 || obj.x >= canvas.width) {
-            obj.speedX = -obj.speedX;
-        }
-        if (obj.y <= 0 || obj.y >= canvas.height) {
-            obj.speedY = -obj.speedY;
-        }
+        if (agent.x <= 0 || agent.x >= canvas.width) agent.speedX *= -1;
+        if (agent.y <= 0 || agent.y >= canvas.height) agent.speedY *= -1;
+        agent.x += agent.speedX;
+        agent.y += agent.speedY;
 
-        obj.x += obj.speedX;
-        obj.y += obj.speedY;
+        ctx.beginPath();
+        ctx.arc(agent.x, agent.y, 20, 0, 2 * Math.PI);
+        const myOrange = 'rgba(255, 166, 55, 0.7)';
 
-        context.beginPath();
-        context.arc(obj.x, obj.y, 10, 0, 2 * Math.PI);
-        context.fillStyle = 'orange';
-        context.fill();
+        ctx.fillStyle = myOrange;
+        ctx.fill();
+        ctx.font = '10px Arial';
+        ctx.fillStyle = 'white';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(agent.number, agent.x, agent.y);
 
-        context.font = '10px Arial';
-        context.fillStyle = 'white';
-        context.textAlign = 'center';
-        context.textBaseline = 'middle';
-        context.fillText(obj.number, obj.x, obj.y);
-    });
-}
-
-
-function drawLetters() {
-    context.font = '48px Arial';
-    context.fillStyle = 'black';
-    context.fillText('A', positionA.x, positionA.y);
-    context.fillText('B', positionB.x, positionB.y);
-    context.fillText('C', positionC.x, positionC.y);
-}
-
-
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-async function takeStep() {
-    async function loopWithDelayAsync(maxIterations) {
-        for (let iteration = 1; iteration <= maxIterations; iteration++) {
-            await delay(100);
-            steps++;
-            document.getElementById("steps").innerHTML = steps;
-            drawObjects();
+        if (agent.nextTarget === 'entrance' && isCloseEnough(agent, positionEntrance)) {
+            agents.splice(i, 1);
         }
     }
-    await loopWithDelayAsync(30)
 }
 
 
+function startSimulation() {
+    startTimer()
+    initAgents();
+    let steps = 0;
+    let interval = setInterval(() => {
+        updateAgents();
+        steps++;
+        if (steps >= numSteps) {
+            clearInterval(interval);
+            pauseTimer()
+        }
+    }, millisecond_pause);
+}
 
-init();
-takeStep();
+
+function drawGrids() {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'black';
+
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.2)';
+    let fitting = {
+        x: 490,
+        y: 28,
+        w: 230,
+        h: 200,
+        centerX: 0,
+        centerY: 0
+    }
+    ctx.fillRect(fitting.x, fitting.y, fitting.w, fitting.h);
+
+   ctx.fillStyle = 'rgba(0, 255, 0, 0.2)';
+    let men = {
+        x: fitting.x + fitting.w,
+        y: 28,
+        w: 250,
+        h: 200 + (fitting.h / 3),
+        centerX: 0,
+        centerY: 0
+    }
+    ctx.fillRect(men.x, men.y, men.w, men.h);
+
+    ctx.fillStyle = 'rgba(0, 255, 255, 0.2)';
+    let entrance = {
+        x: 830,
+        y: 290,
+        w: 140,
+        h: 280,
+        centerX: 0,
+        centerY: 0
+
+    }
+    ctx.fillRect(entrance.x, entrance.y, entrance.w, entrance.h);
+
+    ctx.fillStyle = 'rgba(0, 255, 133, 0.2)';
+    let checkout = {
+        x: men.x,
+        y: fitting.h + 95,
+        w: 120,
+        h: 260,
+        centerX: 0,
+        centerY: 0
+
+    }
+    ctx.fillRect(checkout.x, checkout.y, checkout.w, checkout.h);
+
+
+    ctx.fillStyle = 'rgba(255, 166, 33, 0.2)';
+    let women = {
+        x: fitting.x,
+        y: fitting.y + fitting.h,
+        w: 230,
+        h: 340,
+        centerX: 0,
+        centerY: 0
+
+
+    }
+    ctx.fillRect(women.x, women.y, women.w, women.h);
+
+
+    function setCenters(box) {
+        box.centerX = box.x + box.w / 2
+        box.centerY = box.y + box.h / 2
+        return box
+    }
+    fitting = setCenters(fitting)
+    men = setCenters(men)
+    entrance = setCenters(entrance)
+    checkout = setCenters(checkout)
+    women = setCenters(women)
+
+    
+    areas.push(fitting)
+
+    areas.push(men)
+    areas.push(entrance)
+    areas.push(checkout)
+    areas.push(women)
+}
